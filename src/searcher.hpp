5c9e4afff85218e44a60c2ab75024a1e08661d58
Ryo Murakami <integral14@gmail.com>
2014-12-04 13:10:11 +0900

    separate impl of delauney

--- src/searcher.hpp
+++ src/searcher.hpp
@@ -32,6 +32,75 @@ class SearcherBase {
                       const std::vector<particle_type>& particles) = 0;
 };
 
+namespace {
+
+/**
+ * @TODO call constructor using tuple expansion
+ * @brief generates Point from Vec<T,N>
+ */
+template <class Point, class T, std::size_t N>
+struct VecToPoint;
+
+template <class Point, class T>
+struct VecToPoint<Point, T, 2> {
+  static Point generate(const Vec<T, 2>& vec) {
+    return Point(vec[0], vec[1]);
+  }
+};
+
+template <class Point, class T>
+struct VecToPoint<Point, T, 3> {
+  static Point generate(const Vec<T, 3>& vec) {
+    return Point(vec[0], vec[1], vec[2]);
+  }
+};
+
+template <class Delaunay, class T, std::size_t N, class Iterator>
+void delauney_triangulation_impl(Delaunay& delauney, Iterator first,
+                                 Iterator last) {
+  typedef typename Delaunay::Point Point;
+
+  std::vector<std::pair<Point, std::size_t>> point_info;
+  std::size_t index = 0;
+  for (Iterator it = first; it != last; ++it) {
+    const auto& pos = it->position();
+    point_info.emplace_back(VecToPoint<Point, T, N>::generate(pos), index++);
+  }
+  delauney.clear();
+  delauney.insert(point_info.begin(), point_info.end());
+}
+
+
+/**
+ * @tparam Delaunay
+ * @tparam AdjacentList
+ * @tparam Particles
+ */
+template <class Delaunay, class AdjacentList, class Particles>
+void delauney_search_impl(Delaunay& delauney, AdjacentList& adjacency_list,
+                        const Particles& particles) {
+  std::vector<typename Delaunay::Vertex_handle> adjacent_vertices(particles.size());
+
+  // Loop for all vertices
+  auto vit = delauney.finite_vertices_begin();
+  while (vit != delauney.finite_vertices_end()) {
+    auto& neighbors = adjacency_list[vit->info()];
+    neighbors.clear();
+
+    // Loop for adjacent vertices of the vertex
+    auto adjacent_end =
+        delauney.finite_adjacent_vertices(vit, adjacent_vertices.begin());
+    auto it = adjacent_vertices.begin();
+    while (it != adjacent_end) {
+      neighbors.push_back(&particles[0]);
+      ++it;
+    }
+    ++vit;
+  }
+}
+
+}  // namespace
+
 template <class T, std::size_t N>
 class DelauneySearcher;
 
@@ -50,26 +119,10 @@ class DelauneySearcher<T, 3> : public SearcherBase<T, 3> {
 
   void search(adjacency_list_type& adjacency_list,
               const std::vector<particle_type>& particles) {
-    triangulation(particles.begin(), particles.end());
-
-    std::vector<Delaunay::Vertex_handle> adjacent_vertices(particles.size());
-
-    // Loop for all vertices
-    auto vit = delauney_.finite_vertices_begin();
-    while (vit != delauney_.finite_vertices_end()) {
-      auto& neighbors = adjacency_list[vit->info()];
-      neighbors.clear();
-
-      // Loop for adjacent vertices of the vertex
-      auto adjacent_end =
-          delauney_.finite_adjacent_vertices(vit, adjacent_vertices.begin());
-      auto it = adjacent_vertices.begin();
-      while (it != adjacent_end) {
-        neighbors.push_back(&particles[0]);
-        ++it;
-      }
-      ++vit;
-    }
+    delauney_triangulation_impl<Delaunay, T, 3>(delauney_, particles.begin(),
+                                                particles.end());
+    // triangulation(particles.begin(), particles.end());
+    delauney_search_impl(delauney_, adjacency_list, particles);
   }
 
  private:
@@ -82,8 +135,7 @@ class DelauneySearcher<T, 3> : public SearcherBase<T, 3> {
     for (Iterator it = first; it != last; ++it) {
       const auto& pos = it->position();
       // TODO: create vec without specify members x, y, z
-      point_info.emplace_back(
-          Point(pos[0], pos[1], pos[2]), index++);
+      point_info.emplace_back(VecToPoint<Point, T, 3>::generate(pos), index++);
     }
     delauney_.clear();
     delauney_.insert(point_info.begin(), point_info.end());
