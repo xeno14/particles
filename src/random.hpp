564b258ed57c98f728d890ddcd94cd59c511e407
Ryo Murakami <integral14@gmail.com>
2014-12-11 22:29:07 +0900

    value type

--- src/random.hpp
+++ src/random.hpp
@@ -169,7 +169,7 @@ struct IsotoropicRand<T, 2, Engine> {
 template <class T, class Engine>
 struct IsotoropicRand<T, 3, Engine> {
   struct Expression {
-    typedef value_type T;
+    typedef T value_type;
     T x[3];
 
     Expression(T r) {
