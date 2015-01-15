863462f718df3df098d2e5b1d5b0461838e4e9bb
Ryo Murakami <integral14@gmail.com>
2015-01-15 14:11:18 +0900

    istream for vec and test using TEST_F

--- test/io_test.cpp
+++ test/io_test.cpp
@@ -8,79 +8,101 @@
 
 using namespace particles;
 
-void clear_ss(std::stringstream& ss) {
-  ss.str("");
-  ss.clear(std::stringstream::goodbit);
-}
-
-TEST(IoTest, output) {
+class IOTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    ss.str("");
+    ss.clear(std::stringstream::goodbit);
+  }
   std::stringstream ss;
-  
+};
+
+TEST_F(IOTest, output_1) {
   auto t = std::make_tuple(1, 2, 3);
-  
   io::output<3>(ss, t);
   EXPECT_EQ("1 2 3", ss.str());
-  clear_ss(ss);
+}
 
+TEST_F(IOTest, output_2) {
+  auto t = std::make_tuple(1, 2, 3);
   io::output<3>(ss, t, "  ");
   EXPECT_EQ("1  2  3", ss.str());
-  clear_ss(ss);
+}
 
+TEST_F(IOTest, output_3) {
+  auto t = std::make_tuple(1, 2, 3);
   io::output<3>(ss, t, ",", "(", ")");
   EXPECT_EQ("(1,2,3)", ss.str());
-  clear_ss(ss);
+}
 
+TEST_F(IOTest, output_vec) {
   Vec<int, 3> v {1, 2, 3};
   io::output<3>(ss, v);
   EXPECT_EQ("1 2 3", ss.str());
-  clear_ss(ss);
 }
 
-TEST(IoTest, output_particle) {
-  std::stringstream ss;
-
+TEST_F(IOTest, output_particle) {
   Particle<int, 2> p({1,2},{3,4},5);
-
   io::output_particle(ss, p, ",");
   EXPECT_EQ("1,2,3,4", ss.str());
-  clear_ss(ss);
 }
 
-TEST(IoTest, Vec_ostream){
-  std::stringstream ss;
-
+TEST_F(IOTest, Vec_ostream){
   Vec<int, 3> v {1, 2, 3};
-
-  ss << v;
-  EXPECT_EQ("[1, 2, 3]", ss.str());
-  clear_ss(ss);
-
-  ss << v << "@" << v;
-  EXPECT_EQ("[1, 2, 3]@[1, 2, 3]", ss.str());
-  clear_ss(ss);
+  ss << v << "@";
+  EXPECT_EQ("[1, 2, 3]@", ss.str());
 }
 
-TEST(IoTest, output_particles) {
-  std::stringstream ss;
-
+class OutputParticlesTest : public IOTest {
+ protected:
+  virtual void SetUp() {
+    IOTest::SetUp();
+    v.push_back(Particle<int, 2>({1, 2}, {3, 4}));
+    v.push_back(Particle<int, 2>({5, 6}, {7, 8}));
+  }
+  virtual void TearDown() {
+    v.clear();
+  }
   std::vector<Particle<int, 2>> v;
-  v.push_back(Particle<int, 2>({1, 2}, {3, 4}));
-  v.push_back(Particle<int, 2>({5, 6}, {7, 8}));
+};
 
+TEST_F(OutputParticlesTest, no_sep) {
   io::output_particles(ss, v.begin(), v.end());
   EXPECT_EQ("1 2 3 4\n5 6 7 8\n", ss.str());
-  clear_ss(ss);
-
-  io::output_particles(ss, v.begin(), v.end(), ",", "@");
-  EXPECT_EQ("1,2,3,4@5,6,7,8@", ss.str());
 }
 
-TEST(IoTest, Particle_ostream) {
-  std::stringstream ss;
+TEST_F(OutputParticlesTest, with_sep) {
+  io::output_particles(ss, v.begin(), v.end(), ",", "@") << "*";
+  EXPECT_EQ("1,2,3,4@5,6,7,8@*", ss.str());
+}
 
+TEST_F(IOTest, Particle_ostream) {
   Particle<int, 2> p({1,2},{3,4},5);
+  ss << p << "@";
+  EXPECT_EQ("{[1, 2], [3, 4], 5}@", ss.str());
+}
 
-  ss << p;
-  EXPECT_EQ("{[1, 2], [3, 4], 5}", ss.str());
-  clear_ss(ss);
+TEST_F(IOTest, vec_istream) {
+  Vec<int, 3> v;
+  int n;
+
+  ss << "1 2 3 4";
+  ss >> v >> n;
+  EXPECT_EQ(1, v[0]);
+  EXPECT_EQ(2, v[1]);
+  EXPECT_EQ(3, v[2]);
+  EXPECT_EQ(4, n);
+}
+
+TEST_F(IOTest, vec_istream2) {
+  Vec<int, 2> u;
+  Vec<int, 3> v;
+
+  ss << "1 2 3 4 5";
+  ss >> u >> v;
+  EXPECT_EQ(1, u[0]);
+  EXPECT_EQ(2, u[1]);
+  EXPECT_EQ(3, v[0]);
+  EXPECT_EQ(4, v[1]);
+  EXPECT_EQ(5, v[2]);
 }
