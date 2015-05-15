b6f8402cda516da13eeeacd858e22d28b2784fe0
Ryo Murakami <integral14@gmail.com>
2015-05-15 23:28:50 +0900

    xrange

--- include/particles/range.hpp
+++ include/particles/range.hpp
@@ -14,6 +14,7 @@
 #include "io.hpp"
 #include "range/zip.hpp"
 #include "range/enumerate.hpp"
+#include "range/xrange.hpp"
 
 #include <functional>
 #include <iterator>
@@ -22,61 +23,6 @@
 
 namespace particles {
 namespace range {
-
-/**
- * @brief suite last to make sure counting up by step does not skip over
- *
- * @tparam T integral_type
- */
-template <class T>
-T suite_last(T first, T last, T step) {
-  if ((last - first) % step != 0) {
-    return last + step - (last - first) % step;
-  }
-  return last;
-}
-
-template <class T=int>
-class XRange {
-  public:
-    class iterator : public std::iterator<std::input_iterator_tag, T> {
-      public:
-        iterator(T i, T step) : count_(i), step_(step) {}
-        iterator(const iterator& it) : count_(it.count_), step_(it.step_) {}
-        iterator& operator++() {
-          count_ += step_;
-          return *this;
-        }
-        iterator operator++(int) {
-          iterator old(*this);
-          operator++();
-          return old;
-        }
-        iterator& operator=(const iterator& it) {
-          count_ = it.count_; step_ = it.step_;
-          return *this;
-        }
-        bool operator==(const iterator& it) const {
-          return count_ == it.count_;
-        }
-        bool operator!=(const iterator& it) const {
-          return count_ != it.count_;
-        }
-        T operator*() const { return count_; }
-      private:
-        T count_, step_;
-    };
-    
-    XRange(T first, T last, T step=1) :
-        first_(first), last_(suite_last(first, last, step)), step_(step) {}
-    auto begin() { return iterator(first_, step_); }
-    auto end() { return iterator(last_, step_); }
-
-  private:
-    T first_, last_, step_;
-};
-
-
 
 /**
  * @brief wrap a iteretor and operator* calls a function
@@ -270,25 +216,6 @@ inline auto average(InputIterator first, InputIterator last) {
   s /= double(num);
   return s;
   /** @todo return s / double(num); why it does not work for Vec? */
-}
-
-/**
- * @brief python-like xrange
- *
- * @code
- * // 0, 2, 4
- * for (auto n : xrange(0, 6, 2)) {
- *   ...
- * }
- * // 0, 2, 4
- * for (auto n : xrange(0, 5, 2)) {
- *   ...
- * }
- * @pre first <= last
- */
-template <class T=int>
-inline auto xrange(T first, T last, T step=1) {
-  return range::XRange<T>(first, last, step);
 }
 
 /**
